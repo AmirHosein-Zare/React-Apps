fdde07ec9e7301ebec54da406b37f9ace9c02958
AmirHosein <awir.software@gmail.com>
2022-12-27 23:59:31 +0330

    Add Info section ti site

--- src/Info/Info.js
+++ src/Info/Info.js
@@ -5,7 +5,10 @@ import Content from './content'
 export default function Info() {
   return (
     <div className='Info'>
-        <Content num={1} title="" />
+        <Content num={1158} title="کاربران ما" />
+        <Content num={1670} title="پروژه ها" />
+        <Content num={20} title="سال های تجربه" />
+        <Content num={18} title="جوایز" />
     </div>
   )
 }
